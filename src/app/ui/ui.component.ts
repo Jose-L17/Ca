70a0bf6c03f94ae14cf836514070c5f067ca468a
Jose-L17 <100440883+Jose-L17@users.noreply.github.com>
2023-11-08 20:38:27 -0600

    Update ui.component.ts #2

--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -1,17 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { addition } from "../addition/addition"
-import { substraction } from '../substraction/substraction';
-import { multiplication } from '../multiplication/multiplication';
-import { division } from '../division/division';
-import { exp } from '../exp/exp';
-import { sqr } from '../sqr/sqr';
-import { sqrt } from '../sqrt/sqrt';
 
 import { modulo } from '../modulo/modulo';
 import { maximo } from '../maximo/maximo';
-import { minimo } from '../minimo/minimo';
-import { logaritmo } from '../logaritmo/logaritmo';
-import { media } from '../media/media';
+
 
 @Component({
   selector: 'app-ui',
@@ -47,30 +38,5 @@ export class UiComponent implements OnInit {
     this.Operacion = " Max "
     this.text = "El Maximo es: " + this.result;
   }
-
-  Minimo() {
-    let myresult = 1;
-    myresult = minimo(this.operator1, this.operator2);
-    this.result = myresult;
-    this.Operacion = " Min "
-    this.text = "El Minimo es: " + this.result;
-  }
-
-  Media() {
-    let myresult = 1;
-    myresult = media(this.operator1, this.operator2);
-    this.result = myresult;
-    this.Operacion = " Media "
-    this.text = "La Media es: " + this.result;
-  }
-
-  Logaritmo() {
-    let myresult = 1;
-    myresult = logaritmo(this.operator1, this.operator2);
-    this.result = myresult;
-    this.Operacion = " Log "
-    this.text = "El Logaritmo es: " + this.result;
-  }
-
 
 }
